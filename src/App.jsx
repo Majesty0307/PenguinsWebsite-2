63b39f8ffcb8a11c26043c9d3d0ee3406a421abc
lightup0101 <31225421-lightup0101@users.noreply.replit.com>
2024-04-22 11:24:51 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+function HomePage() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <h1 style={{ fontFamily: '"Brush Script MT", cursive', color: '#ceabff' }}>
+        {"Majesty's website"}
+      </h1>
+      <h2
+        style={{
+          fontFamily: '"Brush Script MT", cursive',
+          color: '#aecdf5',
+          marginBottom: '10rem'
+        }}
+      >
+        click the buttons below
+      </h2>
+      <button
+        style={{
+          fontFamily: 'Garamond, serif',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: '#8aa1d1',
+          color: 'white'
+        }}
+        onClick={() => alert('Hello there')}
+      >
+        Welcome
+      </button>
+      <button
+        style={{
+          fontFamily: 'Garamond, serif',
+          padding: '1rem',
+          fontSize: '2rem',
+          background: '#928ad1',
+          color: 'white',
+          marginTop: '2rem'
+        }}
+        onClick={() => {
+          const name = prompt("What's your name?");
+          if (name) {
+            alert('Nice to meet you, ' + name + '!');
+          } else {
+            alert('Nice to meet you, stranger');
+          }
+        }}
+      >
+        {"What's your name?"}
+      </button>
+    </div>
+  );
+}
+
+export default HomePage;
